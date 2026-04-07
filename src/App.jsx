143a0360b90b999d30625079722d17abe737edde
shatha-naji <s202279500@kfupm.edu.sa>
2026-04-07 20:37:06 +0300

    add logout button to profile and update routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ import Favorites from "./pages/Favorites";
 import Profile from "./pages/Profile";
 import MealPlan from "./pages/MealPlan";
 import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import AdminDashboard from "./pages/AdminDashboard";
+import ManageUsers from "./pages/ManageUsers";
+import ManageFilters from "./pages/ManageFilters";
+import ManageComments from "./pages/ManageComments";
+import ReviewReports from "./pages/ReviewReports";
+import RecipeLimits from "./pages/RecipeLimits";
 
 function App() {
   return (
@@ -17,6 +25,16 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/meal-planning" element={<MealPlan />} />
         <Route path="/contact" element={<ContactUs />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/users" element={<ManageUsers />} />
+        <Route path="/admin/filters" element={<ManageFilters />} />
+        <Route path="/admin/comments" element={<ManageComments />} />
+        <Route path="/admin/reports" element={<ReviewReports />} />
+        <Route path="/admin/recipe-limits" element={<RecipeLimits />} />
       </Routes>
     </>
   );
