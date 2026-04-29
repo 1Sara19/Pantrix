b1eddfa643b8e879cb3d4fab6cc3f705a2624d35
ReemaAlqahtani1 <s202280860@kfupm.edu.sa>
2026-04-30 00:35:25 +0300

    CRUD

---
+++ backend/controllers/recipeController.js
@@ -0,0 +1,77 @@
+import Recipe from "../models/Recipe.js";
+
+
+// GET all recipes
+export const getRecipes = async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.json(recipes);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// GET recipe by ID
+export const getRecipeById = async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    res.json(recipe);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// POST add new recipe
+export const createRecipe = async (req, res) => {
+  try {
+    const newRecipe = new Recipe(req.body);
+    const savedRecipe = await newRecipe.save();
+
+    res.status(201).json(savedRecipe);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+// PUT update recipe
+export const updateRecipe = async (req, res) => {
+  try {
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedRecipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    res.json(updatedRecipe);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+// DELETE recipe
+export const deleteRecipe = async (req, res) => {
+  try {
+    const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id);
+
+    if (!deletedRecipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    res.json({ message: "Recipe deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
