37c61992288527f1cacee82dcc2d0b22a747f4d6
ReemaAlqahtani1 <s202280860@kfupm.edu.sa>
2026-04-30 00:37:06 +0300

    routes

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,9 +1,21 @@
 import express from "express";
+import {
+  getRecipes,
+  getRecipeById,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe,
+} from "../controllers/recipeController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json([]);
-});
+router.route("/")
+  .get(getRecipes)
+  .post(createRecipe);
+
+router.route("/:id")
+  .get(getRecipeById)
+  .put(updateRecipe)
+  .delete(deleteRecipe);
 
 export default router;
